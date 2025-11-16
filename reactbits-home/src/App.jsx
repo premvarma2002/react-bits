1fa4f172455672ebe32eed3e25fd68fd2145183c
premvarma2002 <vakilverma1982@gmail.com>
2025-11-16 20:13:24 +0530

    basic setup complete

---
+++ reactbits-home/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import DarkVeil from './components/ui/Darkveil'
+
+
+const App = () => {
+  return (
+
+<div style={{ width: '100%', height: '800px', position: 'relative' }}>
+  <DarkVeil />
+</div>
+  )
+}
+
+export default App
